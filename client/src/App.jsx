67ba9ca3f9542ef92d042de0f5ac2422074ee868
ValeriCodeU <valeri.yanev@icloud.com>
2025-11-30 23:31:38 +0200

    Add core React components and initial styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,29 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Pagination from "./components/Pagination"
+import Search from "./components/Search"
+import TableComponent from "./components/TableComponent"
 
 function App() {
 
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <Header />
+            
+            <main className="main">
+                <section className="card users-container">
+                    <Search />
+
+                    <TableComponent />
+
+                    <button className="btn-add btn">Add new user</button>
+
+                    <Pagination />
+                </section>
+
+            </main>
+
+            <Footer />
         </div>
     )
 }
